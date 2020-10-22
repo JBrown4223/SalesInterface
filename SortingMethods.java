14200a0342d96e93d606fbc18b3542ea6454bf09
JBrown4223 <49790807+JBrown4223@users.noreply.github.com>
2020-10-22 10:15:00 -0400

    Delete SortingMethods.java

--- SortingMethods.java
+++
@@ -1,8 +0,0 @@
-public interface SortingMethods {
-
-    //These two methods work together to provide mergesort on the Sales Data
-    void merge(Sales[] array, int low, int mid, int high);
-
-    void mergeSort(Sales[] array, int low, int high);
-
-}
